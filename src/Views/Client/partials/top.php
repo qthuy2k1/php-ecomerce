af2ec856470d0f2dbdb36b3e0fba68c5198d6ef0
qtplus123 <qthuy46@gmail.com>
2022-05-26 17:42:26 +0700

    update

--- src/Views/Client/partials/top.php
+++ src/Views/Client/partials/top.php
@@ -3,5 +3,5 @@ require './src/Views/Client/partials/top/layout.php';
 require './src/Views/Client/auth/modal.php';
 require './src/Views/Client/partials/top/header.php';
 require './src/Views/Client/partials/top/navbar.php';
-require './src/Core/App.php';
+require './src/Helpers/App.php';
 ?>
